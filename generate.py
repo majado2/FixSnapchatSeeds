6ed4ee60c8e64595cc288559d49068c0c733ec8d
abdulmajeed aldhasi <majado22@gmail.com>
2023-07-28 00:45:07 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,25 +1,27 @@
+from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 
-def generate():
-    # سحب الملف من الرابط
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    with open('new_file.xml', 'r', encoding='utf-8') as file:
+        data = file.read()
+    return data
+
+@app.route('/update')
+def update():
     response = requests.get('https://rodastore.com/feeds/snapchat/7RKnzAbjZHnYTJRoAI8KnQ5ELcfrsbEo')
     data = response.text
-
-    # تحليل الملف
     soup = BeautifulSoup(data, 'xml')
-
-    # البحث عن تاق <g:item_group_id> واستبدال محتواه بالرقم 505820
     for tag in soup.find_all('g:item_group_id'):
         tag.string = '505820'
-
-    # البحث عن تاق <g:google_product_category> واستبدال محتواه بالقيمة المطلوبة
     for tag in soup.find_all('g:google_product_category'):
         tag.string = 'Health & Beauty > Health Care > Conductivity Gels & Lotions'
-
-    # حفظ التغييرات في ملف جديد
     with open('new_file.xml', 'w', encoding='utf-8') as file:
         file.write(str(soup.prettify()))
+    return 'Update completed'
 
 if __name__ == "__main__":
-    generate()
+    app.run(host='0.0.0.0', port=8080)
