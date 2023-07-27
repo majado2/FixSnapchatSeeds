55b14612f06beb8af88b8a5c989da097cb5826b3
abdulmajeed aldhasi <majado22@gmail.com>
2023-07-28 00:16:37 +0300

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+def generate():
+    # سحب الملف من الرابط
+    response = requests.get('https://rodastore.com/feeds/snapchat/7RKnzAbjZHnYTJRoAI8KnQ5ELcfrsbEo')
+    data = response.text
+
+    # تحليل الملف
+    soup = BeautifulSoup(data, 'xml')
+
+    # البحث عن تاق <g:item_group_id> واستبدال محتواه بالرقم 505820
+    for tag in soup.find_all('g:item_group_id'):
+        tag.string = '505820'
+
+    # البحث عن تاق <g:google_product_category> واستبدال محتواه بالقيمة المطلوبة
+    for tag in soup.find_all('g:google_product_category'):
+        tag.string = 'Health & Beauty > Health Care > Conductivity Gels & Lotions'
+
+    # حفظ التغييرات في ملف جديد
+    with open('new_file.xml', 'w', encoding='utf-8') as file:
+        file.write(str(soup.prettify()))
+
+if __name__ == "__main__":
+    generate()
